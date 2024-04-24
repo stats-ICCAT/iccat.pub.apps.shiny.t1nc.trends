bf4a90588e51aa9aebe40e6bcaf37370ca044332
Fabio Fiorellato <flowerside@gmail.com>
2024-04-24 10:08:12 +0200

    Minimal edits

--- initialize_data.R
+++ initialize_data.R
@@ -2,7 +2,7 @@ library(iccat.dev.data)
 
 LAST_N_YEARS = 100
 
-NC_ALL = t1nc(db_connection    = DB_T1(username = Sys.getenv("DB_USERNAME"),
-                                       password = Sys.getenv("DB_PASSWORD")))
+NC_ALL = t1nc(db_connection = DB_T1(username = Sys.getenv("DB_USERNAME"),
+                                    password = Sys.getenv("DB_PASSWORD")))
 
 save(list = "NC_ALL", file = "./shiny/NC_all.RData")
