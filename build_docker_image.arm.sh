e6637e41629745c77584de3af36485a4cde9e888
Fabio Fiorellato <fabio@fiorella.to>
2024-10-09 16:35:49 +0200

    Changing docker image name

--- build_docker_image.arm.sh
+++ build_docker_image.arm.sh
@@ -1,4 +1,4 @@
-docker build -t iccat/apps/trends:0.1.0 \
+docker build -t iccat/apps/nc_trends:0.1.0 \
              --build-arg GITHUB_AUTH_TOKEN=$GITHUB_AUTH_TOKEN \
              --progress=plain \
              -f Dockerfile.arm .
